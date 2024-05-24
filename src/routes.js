ff3cef2ac4bf3d1220be4aded15f95460ea3479a
syahrana <syahranapinem24@gmail.com>
2024-05-24 12:33:07 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,35 @@
+/* eslint-disable quotes */
+
+const { 
+    addBookHandler, getAllBookHandler, getBookByIdHandler, updateBookByIdHandler, deleteBookByIdHandler
+  } = require('./handler');
+  
+  const routes = [
+    {
+      method: 'POST',
+      path: '/books',
+      handler: addBookHandler,
+    },
+    {
+      method: 'GET',
+      path: '/books',
+      handler: getAllBookHandler,
+    },
+    {
+      method: 'GET',
+      path: '/books/{bookId}',
+      handler: getBookByIdHandler,
+    },
+    {
+      method: 'PUT',
+      path: '/books/{bookId}',
+      handler: updateBookByIdHandler,
+    },
+    {
+      method: 'DELETE',
+      path: '/books/{bookId}',
+      handler: deleteBookByIdHandler,
+    },
+  ];
+     
+  module.exports = routes;
